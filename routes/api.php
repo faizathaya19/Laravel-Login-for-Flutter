dbda923b06ee7d725a574dbc7884f3843bf601ae
Faiz Athaya Ramadhan <76005226+faizathaya19@users.noreply.github.com>
2023-05-11 15:32:47 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'Register']);
+Route::get('/login', [AuthController::class, 'Login']);
